4cd56a900487beca5c2dba4e97bbbed8f754d43a
Giovanni Carlino <97813246+giovannicarlino04@users.noreply.github.com>
2024-09-14 23:56:15 +0200

    I added too many things at the same time

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,15 +1,9 @@
 #version 330 core
-in vec3 FragPos;
-in vec3 Normal;
-in vec3 ourColor;
-
 out vec4 FragColor;
 
-uniform vec3 lightDir; // Direction of the light
-uniform vec3 lightColor; // Color of the light
+in vec3 ourColor;
 
-void main() {
-    float diff = max(dot(normalize(Normal), -lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
-    FragColor = vec4(diffuse * ourColor, 1.0);
+void main()
+{
+    FragColor = vec4(ourColor, 1.0);
 }
