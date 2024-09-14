05c8326cd770dcb6c3a80e9203c489b02155fb6f
Giovanni Carlino <97813246+giovannicarlino04@users.noreply.github.com>
2024-09-14 23:59:41 +0200

    heee yeah now it works as intended

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -1,9 +1,15 @@
 #version 330 core
-out vec4 FragColor;
-
+in vec3 FragPos;
+in vec3 Normal;
 in vec3 ourColor;
 
-void main()
-{
-    FragColor = vec4(ourColor, 1.0);
+out vec4 FragColor;
+
+uniform vec3 lightDir; // Direction of the light
+uniform vec3 lightColor; // Color of the light
+
+void main() {
+    float diff = max(dot(normalize(Normal), -lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+    FragColor = vec4(diffuse * ourColor, 1.0);
 }
--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -2,14 +2,17 @@
 layout(location = 0) in vec3 aPos;
 layout(location = 1) in vec3 aColor;
 
+out vec3 FragPos;
+out vec3 Normal;
 out vec3 ourColor;
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-void main()
-{
+void main() {
+    FragPos = vec3(model * vec4(aPos, 1.0));
+    Normal = mat3(transpose(inverse(model))) * aColor;
     gl_Position = projection * view * model * vec4(aPos, 1.0);
     ourColor = aColor;
 }
